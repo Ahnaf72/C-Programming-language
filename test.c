275ab624168174b1fc583303a27bd99ed45f22d4
Ahnaf72 <ahnafnehal981@gmail.com>
2023-04-01 12:37:26 +0600

    Test

---
+++ test.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include <math.h>
+
+int main (){
+    int a, b , sum = 0;
+    printf("Enter a number: \n");
+    scanf("%d", & a);
+    printf("Enter a number: \n");
+    scanf("%d", & b);
+
+    sum = a+b;
+    printf("The sum is : %d", sum);
+}
